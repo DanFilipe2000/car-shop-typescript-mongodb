a21f4899f9767f4d4b50dc3518d5a08d663e3a40
Daniel Filipe <anielilipe90@gmail.com>
2022-11-08 16:07:43 -0300

    refactor: IVehicle and ICar, add library Zod

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,6 +1,24 @@
-import { IVehicle } from './IVehicle';
+import { z } from 'zod';
+import { VehicleSchema } from './IVehicle';
 
-export interface ICar extends IVehicle {
-  doorsQty: number;
-  seatsQty: number;
-}
+// import { IVehicle } from './IVehicle';
+
+// export interface ICar extends IVehicle {
+//   doorsQty: number;
+//   seatsQty: number;
+// }
+
+const CarSchema = VehicleSchema.extend({
+  doorsQty: z.number({
+    required_error: 'Doors Qty is required',
+    invalid_type_error: 'Doors Qty must be a number',
+  }).int().min(2).max(4),
+  seatsQty: z.number({
+    required_error: 'Seats Qty is required',
+    invalid_type_error: 'Seats Qty must be a number',
+  }).int().min(2).max(7),
+});
+
+type ICar = z.infer<typeof CarSchema>;
+
+export { ICar, CarSchema };
--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,7 +1,33 @@
-export interface IVehicle {
-  model: string;
-  year: number;
-  color: string;
-  status?: boolean;
-  buyValue: number;
-}
+import { z } from 'zod';
+
+// export interface IVehicle {
+//   model: string;
+//   year: number;
+//   color: string;
+//   status?: boolean;
+//   buyValue: number;
+// }
+
+const VehicleSchema = z.object({
+  model: z.string({
+    required_error: 'Model is required',
+    invalid_type_error: 'Model must be a string',
+  }).min(3, { message: 'Model must be 3 or more characters long' }),
+  year: z.number({
+    required_error: 'Year is required',
+    invalid_type_error: 'Year must be a number',
+  }).int().min(1900).max(2022),
+  color: z.string({
+    required_error: 'Color is required',
+    invalid_type_error: 'Color must be a string',
+  }).min(3, { message: 'Color must be 3 or more characters long' }),
+  status: z.boolean().optional(),
+  buyValue: z.number({
+    required_error: 'Buy value is required',
+    invalid_type_error: 'Buy value must be a number',
+  }).int(),
+});
+
+type IVehicle = z.infer<typeof VehicleSchema>;
+
+export { IVehicle, VehicleSchema };
